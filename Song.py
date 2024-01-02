ed98bdcb9c783d08dd3d2f9e7daf97051aeff0bc
wealthyrequirement05 <152629531+wealthyrequirement05@users.noreply.github.com>
2024-01-02 13:21:47 -0500

    Update Song.py
    Modified the song class to work with the GUI

--- Song.py
+++ Song.py
@@ -1,24 +1,49 @@
-import wave
 import numpy as np
 from scipy.io.wavfile import read
 from scipy.fft import fft
-from music21 import converter, instrument, note, chord
-
-# Step 1: Load the .wav file
+from music21 import note
 
 class Song:
     def __init__(self, filename):
         self.song_name = filename
-        wav_file = wave.open(filename, 'r')
-        smp_rate, amp_data = read(self.song_name)
-        audio_data = np.array(data)
-    def get_notes(self, audio_data):
-        freq_data = fft(audio_data)
+        self.smp_rate, self.audio_data = read(self.song_name)
+
+    def freq_to_midi(self, freq):
+        return np.round(69 + 12 * np.log2(freq / 440))
+
+    def get_notes(self):
+        freq_data = fft(self.audio_data)
+        freqs = np.fft.fftfreq(len(freq_data), 1 / self.smp_rate)
         notes = []
-        for freq in freq_data:
-            n = note.Note(freq) #Here is the map step
-            notes.append(n)
+        for freq in freqs:
+            if 20 <= freq <= 20000:  # Check if the frequency is within the audible range
+                midi_num = self.freq_to_midi(freq)
+                n = note.Note(int(midi_num))  # Here is the map step
+                notes.append(n)
         return notes
+
+    #def get_notes(self):
+        #Here, I'm using a Fourier Transform that returns a complex-valued array. This could have issues for
+        #the loop, so I will attempt to use a modified Fourier Transform later
+        #freq_data = fft(self.audio_data)
+        #notes = []
+
+        #freqs = np.fft.fftfreq(len(freq_data), 1 / self.smp_rate)
+
+
+        #for freq in freqs:
+        #    midi_num = self.freq_to_midi(freq)
+        #    n = note.Note(int(midi_num))  # Here is the map step
+        #notes.append(n)
+        #return notes
+        #for freq in freq_data: #Since I'm using a complex-valued array, I tried below to use the absolute value
+        #for freq in np.abs(freq_data):
+
+        #    midi_num = self.freq_to_midi(freq)
+        #    n = note.Note(midi_num)  # Here is the map step
+        #    notes.append(n)
+        #return notes
+
 
 
     #Below is a possible text display, but I think this should go into another class
@@ -40,3 +65,11 @@ class Song:
     #def modify_notes(input_from_UI):
         #The way this should work is that when the user clicks a button after the sheet music is change, the
         #modified sheet music should be read by this method and the notes of the song should change.
+
+
+
+
+
+
+
+
